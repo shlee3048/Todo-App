444cdd2142079e55d9f51c96fdc9ece6452d9674
shlee3048 <104054424+shlee3048@users.noreply.github.com>
2022-04-25 14:56:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+aws_db = {
+    "user": "{admin}",
+    "password": "{f4d3s2a1}",
+    "host": "{test.cwdtf0uzwslg.ap-northeast-2.rds.amazonaws.com}",
+    "port": 3306, # Maria DB의 포트
+    "database": "{test}",
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{aws_db['user']}:{aws_db['password']}@{aws_db['host']}:{aws_db['port']}/{aws_db['database']}?charset=utf8"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
